5f89efd9ac0f153194ee6161896e693d44f47f79
HodRZ <hodaifa.radwan@gmail.com>
2022-09-08 20:04:39 +0300

    Add Push method and test

--- src/__test__/list.test.js
+++ src/__test__/list.test.js
@@ -2,7 +2,7 @@
 
 const { SinglyLinkedList } = require('./../modules/List.js');
 
-describe('Test the List', () => {
+describe('List Proof of Life', () => {
     it('should create a list', () => {
         const newList = new SinglyLinkedList
         expect(newList).toBeInstanceOf(SinglyLinkedList)
@@ -10,7 +10,9 @@ describe('Test the List', () => {
         expect(newList.tail).toBeNull()
         expect(newList.length).toEqual(0)
     })
+})
 
+describe('List unshift', () => {
     it('should add a Node as a Head to empty list', () => {
         const newList = new SinglyLinkedList;
         newList.unshift('5');
@@ -31,6 +33,31 @@ describe('Test the List', () => {
         expect(newList.head.next.val).toEqual('second')
         expect(newList.head.next.next.val).toEqual('first')
         expect(newList.tail.val).toEqual('first')
-        expect(newList.tail.next).toBeNull
+        expect(newList.tail.next).toBeNull()
     });
-})
+});
+
+describe('List Push', () => {
+    it('should add a Node as a Tail to empty list', () => {
+        const newList = new SinglyLinkedList;
+        newList.push('5');
+        expect(newList.head.val).toEqual('5');
+        expect(newList.tail.val).toEqual('5');
+        expect(newList.head).toEqual(newList.tail);
+        expect(newList.head.next).toBeNull();
+        expect(newList.length).toEqual(1);
+    });
+
+    it('should add a Node as a Tail to a non-empty list', () => {
+        const newList = new SinglyLinkedList;
+        newList.push('first');
+        newList.push('second');
+        newList.push('third');
+        expect(newList.length).toEqual(3);
+        expect(newList.head.val).toEqual('first')
+        expect(newList.head.next.val).toEqual('second')
+        expect(newList.head.next.next.val).toEqual('third')
+        expect(newList.tail.val).toEqual('third')
+        expect(newList.tail.next).toBeNull()
+    });
+});
--- src/modules/List.js
+++ src/modules/List.js
@@ -9,16 +9,28 @@ class SinglyLinkedList {
     };
 
     unshift(val) {
-        const newHead = new Node(val);
+        const newNode = new Node(val);
         if (!this.head) {
-            this.head = newHead;
-            this.tail = newHead;
+            this.head = newNode;
+            this.tail = newNode;
         } else {
-            newHead.next = this.head;
-            this.head = newHead
+            newNode.next = this.head;
+            this.head = newNode;
         }
         this.length++;
-        return this
+        return this;
+    }
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
     }
 };
 
